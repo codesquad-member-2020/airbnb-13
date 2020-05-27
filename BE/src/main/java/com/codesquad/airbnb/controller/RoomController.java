e91347753961accdea65bacd9af686dae5f6cf6a
koo bohyeon <goosee115@gmail.com>
2020-05-28 02:01:41 +0900

    [#19]refactor: 불필요한 import문 삭제함

--- BE/src/main/java/com/codesquad/airbnb/controller/RoomController.java
+++ BE/src/main/java/com/codesquad/airbnb/controller/RoomController.java
@@ -1,8 +1,5 @@
 package com.codesquad.airbnb.controller;
 
-import com.codesquad.airbnb.dto.Guest;
-import com.codesquad.airbnb.dto.Price;
-import com.codesquad.airbnb.dto.ReservationDate;
 import com.codesquad.airbnb.dto.Room;
 import com.codesquad.airbnb.service.RoomService;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +7,6 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
