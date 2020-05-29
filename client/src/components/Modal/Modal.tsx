2f6f860dc5a3d721f3b31cebdd0d774235655e32
kiyoesjh <hyeyaa125@gmail.com>
2020-05-29 12:52:16 +0900

    [#27] feat: Modal 컴포넌트 스타일 작업
    why: Modal 컴포넌트에 필요한 스타일 작업과 검은 화면 클릭 시 모달이 닫힐 수 있도록 작업했다.

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -1,12 +1,62 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
-import FlexLayout from '@Custom/FlexLayout/FlexLayout';
+import { useDispatch } from 'react-redux';
+import { turnOffModal } from '../../action/modalAction';
+import Icon from '$Icon/Icon';
+import Button from '@Custom/Button/Button';
 
 type modalProp = {
   content: () => JSX.Element | null;
 };
 const Modal = ({ content }: modalProp) => {
-  return <div>{content()}</div>;
+  const dispatch = useDispatch();
+  const onClickHandler = () => dispatch(turnOffModal());
+  return (
+    <div css={modalDim}>
+      <div onClick={onClickHandler} css={overlay}></div>
+
+      <div css={modalWrap}>
+        {content()}
+        <div css={buttonWrapStyle}>
+          <Button theme={'nooutline'} width={'15px'} fontSize={'medium'} onClick={onClickHandler}>
+            <Icon icon="close" color="black" size="15px" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
+
+const modalDim = css`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  top: 0;
+  left: 0;
+`;
+
+const overlay = css`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const modalWrap = css`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: #fff;
+  padding: 1rem;
+`;
+
+const buttonWrapStyle = css`
+  position: absolute;
+  top: 5px;
+  right: 10px;
+`;
