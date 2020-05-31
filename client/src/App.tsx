fcb296650ef3df2576670830fcb7b3f33b964bff
sangwon21 <junothyme@gmail.com>
2020-05-31 11:15:18 +0900

    [#33] feat: 예약 버튼과 예약 모달 연결
    why: 예약 버튼을 눌렀을 때, 해당 숙소 정보를 바탕으로 예약 모달이 나타날
    수 있게끔, 버튼과 모달을 연결했다.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,22 +4,18 @@ import Cards from './components/Cards/Cards';
 import { ThemeProvider } from 'emotion-theming';
 import theme from './style/theme';
 import Modal from '@Modal/Modal';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { RootState } from '@Reducer/index';
-import { turnOnModal } from '@Action/modalAction';
 import Header from '@Header/Header';
 import Filters from '@Filters/Filters';
 
 const App = () => {
   const { on, content } = useSelector((state: RootState) => state.modalReducer);
-  const dispatch = useDispatch();
-  const onClickHandler = () => dispatch(turnOnModal('Reservation'));
 
   return (
     <ThemeProvider theme={theme}>
       <Header />
-      {/* <button onClick={onClickHandler}>click</button>
-      {on && <Modal content={content} />} */}
+      {on && <Modal content={content} />}
       <div css={wrapStyle}>
         <Filters />
         <Cards />
